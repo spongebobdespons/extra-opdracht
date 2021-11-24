45499de5f41d82b4c718017da10dec77728d4375
Luuk Wijnants <90309537+spongebobdespons@users.noreply.github.com>
2021-11-24 09:34:17 +0000

    stap 4

--- script.js
+++ script.js
@@ -9,32 +9,7 @@ class Ball {
  
 }
 
-function draw() {
-	background(225);
-
-  ellipse(x,y,50,50)
-  fill('red')
-  
-  circle(x2,y2,25);
-  
-  x = x + vx;
- // y = y + vy;
-  
-  if(x < 0 || x > 500){
-    vx = vx * -1;
-  }
-
-  if(y < 0 || y > 400){
-    vy = vy * -1;
-  }
-  x2 = x2 + vx2;
-  y2 = y2 + vy2;
-  
-  if(x2 < 0 || x2 > 500){
-    vx2 = vx2 * -1;
-  }
-
-  if(y2 < 0 || y2 > 400){
-    vy2 = vy2 * -1;
-  }
-}
+ draw(){
+  ellipse(this.x,this.y,50,50);
+  this.x = this.x + this.vx;
+  this.y = this.y + this.vy;
