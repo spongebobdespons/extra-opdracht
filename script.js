177b8ff30ad6b0ca47bc60d963096a4cd7628295
Luuk Wijnants <90309537+spongebobdespons@users.noreply.github.com>
2021-11-24 09:20:49 +0000

    stap 1 en 2

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
-let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+var x, y, vx, vy
+var x2, y2, vx2, vy2
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-    background(255);
+	createCanvas(500, 400);
+  
+  vx = 5;
+  vy = 5;
+  y = 200;
+  x = 30;
+  vx2 = 5;
+  vy2 = 5;
+  y2 = 20;
+  x2 = 30;
 }
 
+
+
 function draw() {
-  noStroke()
-  fill(random(colorlist));
-  ellipse(mouseX, mouseY, 25, 25);
+	background(225);
+
+  ellipse(x,y,50,50)
+  fill('red')
+  
+  circle(x2,y2,25);
+  
+  x = x + vx;
+ // y = y + vy;
+  
+  if(x < 0 || x > 500){
+    vx = vx * -1;
+  }
+
+  if(y < 0 || y > 400){
+    vy = vy * -1;
+  }
+  x2 = x2 + vx2;
+  y2 = y2 + vy2;
+  
+  if(x2 < 0 || x2 > 500){
+    vx2 = vx2 * -1;
+  }
+
+  if(y2 < 0 || y2 > 400){
+    vy2 = vy2 * -1;
+  }
 }
